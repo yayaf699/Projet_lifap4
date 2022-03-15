61434a935a124e941b0c64a07e6e475a9ba7b7fb
SIDKI NOUREDDINE p2008210 <noureddine.sidki@etu.univ-lyon1.fr>
2022-03-15 10:11:51 +0000

    Update Partie.h

--- src/Partie.h
+++ src/Partie.h
@@ -1,11 +1,30 @@
 #include <iostream>
+#include <Joueur.h>
 
 struct Partie
 {
     public:
-    bool fini;
-
+        bool fini;
+        bool prochain_joueur;
     private:
-    void prochaine_action();
+        void prochaine_action();
 
 };
+
+void prochaine_action()
+{
+
+}
+
+int main(void)
+{
+    Partie p;
+    p.fini = false;
+    Joueur j1,j2;
+    j1.Joueur("Nordine",5,3,10,5);
+    j2.Joueur("Ennemi",5,3,10,5);
+    while (p.fini == false)
+    {
+        p.prochaine_action();
+    }
+}
