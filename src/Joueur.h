6fb9e9fc9285419d735e0c7965768965cdff438a
ITJI AMINE p2018984 <amine.itji@etu.univ-lyon1.fr>
2022-03-20 22:19:06 +0000

    Replace Joueur.h

--- src/Joueur.h
+++ src/Joueur.h
@@ -1,60 +1,90 @@
 #ifndef JOUEUR_H_INCLUDED
 #define JOUEUR_H_INCLUDED
 #include <string.h>
+#include "Inventaire.h"
 
 using namespace std;
 
 class Joueur
 {
-    private:
 
-        string nom;
-        int *tab; // [0] vie , [1] force , [2] vitesse , [3] endurance
+public:
 
-    public:
+// LES CONSTRUCTEURS ET DESTRUCTEUR
 
-    /** CONSTRUCTEURS ET DESTRUCTEUR **/
+    // CONSTRUCTEUR PAR DEFAUT
+    Joueur();
 
-        // CONSTRUCTEUR
-            Joueur();
+    // CONSTRUCTEUR PERSONNALISÉ
+    Joueur(const string &n, const unsigned char &vi, const unsigned char &f, const unsigned char &v, const unsigned char &e, const Inventaire &in);
 
-        // CONSTRUCTEUR PERSONNALISÉ
-            Joueur(const string &n, const int &vi, const int &f, const int &v, const int &e);
+    // DESTRUCTEUR
+    ~Joueur();
 
-        // DESTRUCTEUR
-            ~Joueur();
+// LES ACCESSEURS ET MUTATEURS 
 
-    /** ACCESSEURS ET MUTATEURS **/
+    // ACCESSEUR NOM
+    const string &getNom() const;
 
-        // ACCESSEUR NOM
-            string getNom();
+    // ACCESSEUR VIE
+    const unsigned char &getVie() const;
 
-        // ACCESSEUR VIE
-            int getVie();
+    // ACCESSEUR FORCE
+    const unsigned char &getForce() const;
 
-        // ACCESSEUR FORCE
-            int getForce();
+    // ACCESSEUR VITESSE
+    const unsigned char &getVitesse() const;
 
-        // ACCESSEUR VITESSE
-            int getVitesse();
+    // ACCESSEUR ENDURANCE
+    const unsigned char &getEndurance() const;
 
-        // ACCESSEUR ENDURANCE
-            int getEndurance();
+    // MUTATEUR NOM
+    void setNom(const string& n);
 
-        // MUTATEUR NOM
-            void setNom(const string& n);
+    // MUTATEUR VIE
+    void setVie(const unsigned char &vi);
 
-        // MUTATEUR VIE
-            void setVie(const int &vi);
+    // MUTATEUR FORCE
+    void setForce(const unsigned char &f);
 
-        // MUTATEUR FORCE
-            void setForce(const int &f);
+    // MUTATEUR VITESSE
+    void setVitesse(const unsigned char &v);
 
-        // MUTATEUR VITESSE
-            void setVitesse(const int &v);
+    // MUTATEUR ENDURANCE
+    void setEndurance(const unsigned char &e);
 
-        // MUTATEUR ENDURANCE
-            void setEndurance(const int &e);
+// GESTION DU JOUEUR 
+
+    // REGARDER L'INVENTAIRE
+    void regarderInventaire() const;
+
+    // REGARDER UN OBJET ET SES STATS
+    void regarderObjet(const unsigned char &i) const;
+
+    // UTLISER UN OBJET
+    void utiliserObjet(const unsigned char &i);
+
+    // REGARDER LES ATTAQUES
+    void regarderLesAttaques() const;
+
+    // REGARDER UNE ATTAQUE ET SES STATS
+    void regarderUneAttaque(const unsigned char &i) const;
+
+    // ATTAQUER
+    void utiliserAttaque(const unsigned char &i);
+
+    // REGARDER SON ARME ET SES STAtS
+    void regarderArme() const;
+
+    // REGARDER SON ARMURE ET SES STATS
+    void regarderArmure() const;
+
+private:
+
+    string nom;
+    unsigned char vie, force, vitesse, endurance;
+    
+    Inventaire *inv; // l'inventaire d'objets du joueur 
 
 };
 
