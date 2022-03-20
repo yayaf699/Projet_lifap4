40bde968f7ce12756b1d86db6b35f84de6d34afc
ITJI AMINE p2018984 <amine.itji@etu.univ-lyon1.fr>
2022-03-20 22:18:17 +0000

    Replace Objet.cpp

--- src/Objet.cpp
+++ src/Objet.cpp
@@ -1,178 +1,61 @@
 #include <iostream>
 #include "Objet.h"
-#include "Joueur.h"
 
 using namespace std;
 
-        /** CONSTRUCTEURS ET DESTRUCTEURS **/
+// LES CONSTRUCTEURS 
 
-        // CONSTRUCTEUR
-            Objet::Objet()
-            {
-                nom = "emplacement vide";
-                tab = new int[4];
+    // CONSTRUCTEUR
+    Objet::Objet()
+    {
+        nom = "emplacement vide";
+        bonusVie = bonusVitesse = bonusForce = bonusEndurance = 0;
+    }
 
-                for(int i = 0; i < 4; i++) tab[i] = 0;
+    // CONSTRUCTEUR STRING
+    Objet::Objet(const string &objet)
+    {
+        // recuperer les données d'un txt
+    }
 
-            }
+    // CONSTRUCTEUR PAR COPIE
+    Objet::Objet(const Objet &o)
+    {
+        nom = o.nom;
+        bonusVie = o.bonusVie;
+        bonusVitesse = o.bonusVitesse;
+        bonusForce = o.bonusForce;
+        bonusEndurance = o.bonusEndurance;
+    }
 
-        // CONSTRUCTEUR STRING
-            Objet::Objet(const string &objet)
-            {
-                // nom = premier mot du string
+// LES ACCESEURS ET MUTATEURS
 
-                // tab[0] = int(deuxieme mot)
-                // tab[1] = int(troisieme mot)
-                // tab[2] = int(quatrieme mot)
-                // tab[3] = int(cinquieme mot)
-            }
+    // ACCESSEUR NOM OBJET
+    const string &Objet::getNomObjet() const { return nom; }
 
-        // CONSTRUCTEUR PAR COPIE
-            Objet(const Objet &o)
-            {
-                nom = objet.nom;
-                for(int i = 0; i < 4; i++) tab[i] = objet.tab[i];
-            }
+    // ACCESSEUR BONUS DE VIE SUR L'OBJET
+    const unsigned char &Objet::getVieObjet() const { return bonusVie; }
 
-        // DESTRUCTEUR
-            Objet::~Objet()
-            {
-                jeterObjet();
-                delete [] tab;
-            }
+    // ACCESSEUR BONUS DE FORCE SUR L'OBJET
+    const unsigned char &Objet::getForceObjet() const { return bonusForce; }
 
-        /** ACCESSEURS ET MUTATEURS **/
+    // ACCESSEUR BONUS DE VITESSE SUR L'OBJET
+    const unsigned char &Objet::getVitesseObjet() const { return bonusVitesse; }
 
-        // ACCESSEUR NOM OBJET
-            string getNomObjet()
-            {
-                return nom;
-            }
+    // ACCESSEUR BONUS DE ENDURANCE SUR L'OBJET
+    const unsigned char &Objet::getEnduranceObjet() const { return bonusEndurance; }
 
-        // ACCESSEUR BONUS DE VIE SUR L'OBJET
-            int getBonusVieObjet()
-            {
-                return tab[0];
-            }
+    // MUTATEUR NOM OBJET
+    void Objet::setNomObjet(const string &n) { nom = n; }
 
-        // ACCESSEUR BONUS DE FORCE SUR L'OBJET
-            int getBonusForceObjet()
-            {
-                return tab[1];
-            }
+    // MUTATEUR BONUS DE VIE SUR L'OBJET
+    void Objet::setVieObjet(const unsigned char &v) { bonusVie = v; }
 
-        // ACCESSEUR BONUS DE VITESSE SUR L'OBJET
-            int getBonusVitesseObjet()
-            {
-                return tab[2];
-            }
+    // MUTATEUR BONUS DE FORCE SUR L'OBJET
+    void Objet::setForceObjet(const unsigned char &f) { bonusForce = f; }
 
-        // ACCESSEUR BONUS DE ENDURANCE SUR L'OBJET
-            int getBonusEnduranceObjet()
-            {
-                return tab[3];
-            }
+    // MUTATEUR BONUS DE VITESSE SUR L'OBJET
+    void Objet::setVitesseObjet(const unsigned char &vi) { bonusVitesse = vi; }
 
-        // MUTATEUR NOM OBJET
-            void setNomObjet(const string &n)
-            {
-                nom = n;
-            }
-
-        // MUTATEUR BONUS DE VIE SUR L'OBJET
-            void setBonusVieObjet(const int &v)
-            {
-                tab[0] = v;
-            }
-
-        // MUTATEUR BONUS DE FORCE SUR L'OBJET
-            void setBonusForceObjet(const int &f)
-            {
-                tab[1] = f;
-            }
-
-        // MUTATEUR BONUS DE VITESSE SUR L'OBJET
-            void setBonusVitesseObjet(const int &vi)
-            {
-                tab[2] = vi;
-            }
-
-        // MUTATEUR BONUS DE ENDURANCE SUR L'OBJET
-            void setBonusEnduranceObjet(const int &e)
-            {
-                tab[3] = e;
-            }
-
-        /** FONCTIONS OBJETS **/
-
-        // AFFICHER OBJET
-            void afficherObjet()
-            {
-                cout<<nom<<" ";
-                cout<<"Bonus vie: +"<<tab[0]<<" ";
-                cout<<"Bonus vitesse: +"<<tab[1]<<" ";
-                cout<<"Bonus force: +"<<tab[2]<<" ";
-                cout<<"Bonus endurance: +"<<tab[3]<<" ";
-                cout<<endl;
-            }
-
-        // UTILISER OBJET
-            void utiliserObjet()
-            {
-                cout<<"Vous utilisez l'objet: "<<nom<<endl;
-
-                // BONUS DE VIE UTILISÉ
-                    if(tab[0] + Joueur::getVie() > 20)
-                    {
-                        Joueur::setVie(20);
-                    }
-                    else
-                    {
-                        Joueur::setVie(tab[0] + Joueur::getVie());
-                    }
-
-                // BONUS DE FORCE UTILISÉ
-                    if(tab[1] + Joueur::getForce() > 20)
-                    {
-                        Joueur::setForce(20);
-                    }
-                    else
-                    {
-                        Joueur::setForce(tab[1] + Joueur::getVie());
-                    }
-
-                // BONUS DE VITESSE UTILISÉ
-                    if(tab[2] + Joueur::getVitesse() > 20)
-                    {
-                        Joueur::setVitesse(20);
-                    }
-                    else
-                    {
-                        Joueur::setVitesse(tab[2] + Joueur::getVie());
-                    }
-
-                // BONUS D'ENDURANCE UTILISÉ
-                    if(tab[3] + Joueur::getEndurance() > 20)
-                    {
-                        Joueur::setEndurance(20);
-                    }
-                    else
-                    {
-                        Joueur::setEndurance(tab[3] + Joueur::getVie());
-                    }
-
-                jeterObjet(); // apres avoir utilisé l'objet il disparait
-            }
-
-        // JETER OBJET
-            void jeterObjet()
-            {
-                nom = "emplacement vide";
-                for(int i = 0; i < 4; i++) tab[i] = 0;
-            }
-
-        // PRENDRE OBJET
-            void prendreObjet(const Objet &o)
-            {
-                Objet(o);
-            }
+    // MUTATEUR BONUS DE ENDURANCE SUR L'OBJET
+    void Objet::setEnduranceObjet(const unsigned char &e) { bonusEndurance = e; }
