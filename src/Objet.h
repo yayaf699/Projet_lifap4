f381027cde35786f0bb2053a0ac9e5800a131c54
ITJI AMINE p2018984 <amine.itji@etu.univ-lyon1.fr>
2022-03-20 22:16:52 +0000

    Replace Objet.h

--- src/Objet.h
+++ src/Objet.h
@@ -4,75 +4,58 @@
 
 using namespace std;
 
-// include .txt de tous les objets
 
 class Objet
 {
+    
+public:
 
-    private:
-        string nom; // nom de l'objet
-        int *tab; // [0] bonus de vie , [1] bonus de force , [2] bonus de vitesse , [3] bonus de endurance
+// LES CONSTRUCTEURS 
 
-    public:
-        /** CONSTRUCTEURS ET DESTRUCTEURS **/
+    // CONSTRUCTEUR
+    Objet();
 
-        // CONSTRUCTEUR
-            Objet();
+    // CONSTRUCTEUR STRING
+    Objet(const string &objet);
 
-        // CONSTRUCTEUR STRING
-            Objet(const string &objet);
+    // CONSTRUCTEUR PAR COPIE
+    Objet(const Objet &o);
 
-        // CONSTRUCTEUR PAR COPIE
-            Objet(const Objet &o);
+// LES ACCESEURS ET MUTATEURS
 
-        // DESTRUCTEUR
-            ~Objet();
+    // ACCESSEUR NOM OBJET
+    const string &getNomObjet() const;
 
-        /** ACCESSEURS ET MUTATEURS **/
+    // ACCESSEUR BONUS DE VIE SUR L'OBJET
+    const unsigned char &getVieObjet() const;
 
-        // ACCESSEUR NOM OBJET
-            string getNomObjet();
+    // ACCESSEUR BONUS DE FORCE SUR L'OBJET
+    const unsigned char &getForceObjet() const;
 
-        // ACCESSEUR BONUS DE VIE SUR L'OBJET
-            int getBonusVieObjet();
+    // ACCESSEUR BONUS DE VITESSE SUR L'OBJET
+    const unsigned char &getVitesseObjet() const;
 
-        // ACCESSEUR BONUS DE FORCE SUR L'OBJET
-            int getBonusForceObjet();
+    // ACCESSEUR BONUS DE ENDURANCE SUR L'OBJET
+    const unsigned char &getEnduranceObjet() const;
 
-        // ACCESSEUR BONUS DE VITESSE SUR L'OBJET
-            int getBonusVitesseObjet();
+    // MUTATEUR NOM OBJET
+    void setNomObjet(const string &n);
 
-        // ACCESSEUR BONUS DE ENDURANCE SUR L'OBJET
-            int getBonusEnduranceObjet();
+    // MUTATEUR BONUS DE VIE SUR L'OBJET
+    void setVieObjet(const unsigned char &v);
 
-        // MUTATEUR NOM OBJET
-            void setNomObjet(const string &n);
+    // MUTATEUR BONUS DE FORCE SUR L'OBJET
+    void setForceObjet(const unsigned char &f);
 
-        // MUTATEUR BONUS DE VIE SUR L'OBJET
-            void setBonusVieObjet(const int &v);
+    // MUTATEUR BONUS DE VITESSE SUR L'OBJET
+    void setVitesseObjet(const unsigned char &vi);
 
-        // MUTATEUR BONUS DE FORCE SUR L'OBJET
-            void setBonusForceObjet(const int &f);
+    // MUTATEUR BONUS DE ENDURANCE SUR L'OBJET
+    void setEnduranceObjet(const unsigned char &e);
 
-        // MUTATEUR BONUS DE VITESSE SUR L'OBJET
-            void setBonusVitesseObjet(const int &vi);
-
-        // MUTATEUR BONUS DE ENDURANCE SUR L'OBJET
-            void setBonusEnduranceObjet(const int &e);
-
-        /** FONCTIONS OBJETS **/
-
-        // AFFICHER OBJET
-            void afficherObjet();
-
-        // UTILISER OBJET
-            void utiliserObjet();
-
-        // JETER OBJET
-            void jeterObjet();
-
-        // PRENDRE OBJET
-            void prendreObjet(const Objet &objet);
+private:
+    string nom; // nom de l'objet
+    unsigned char bonusVie, bonusForce, bonusVitesse, bonusEndurance;
 
 };
 
