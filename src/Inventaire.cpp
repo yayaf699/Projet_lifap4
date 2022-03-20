be91549c39bd78fe2aa840ab29a0bc959c7835cf
ITJI AMINE p2018984 <amine.itji@etu.univ-lyon1.fr>
2022-03-20 22:20:17 +0000

    Replace Inventaire.cpp

--- src/Inventaire.cpp
+++ src/Inventaire.cpp
@@ -1,153 +1,89 @@
 #include <iostream>
 #include <string>
-#include <assert.h>
+#include <cassert>
 #include "Inventaire.h"
-#include "Objet.h"
 
 using namespace std;
 
+// CONSTRUCTEURS
 
-    /** CONSTRUCTEURS ET DESTRUCTEUR **/
+    // CONSTRUCTEUR
+    Inventaire::Inventaire()
+    {
+        inv = new Objet[taille_inventaire];  
+    }
 
-        // CONSTRUCTEUR
-            Inventaire::Inventaire()
+    // CONSTRUCTEUR PAR COPIE
+    Inventaire::Inventaire(const Inventaire &in)
+    {
+        inv = new Objet[in.taille_inventaire];
+        for(unsigned char i = 0; i < taille_inventaire; i++)
+        {
+            inv[i].setNomObjet(in.inv[i].getNomObjet());
+            inv[i].setVieObjet(in.inv[i].getVieObjet());
+            inv[i].setForceObjet(in.inv[i].getForceObjet());
+            inv[i].setVitesseObjet(in.inv[i].getVitesseObjet());
+            inv[i].setEnduranceObjet(in.inv[i].getEnduranceObjet());
+
+        } 
+    }
+
+// GESTION DE L'INVENTAIRE 
+
+    // AFFICHER I-EME OBJET
+    void Inventaire::afficherObjetInventaire(const unsigned char &i) const
+    {
+        assert(i > 0);
+        assert(i < taille_inventaire);
+
+        cout<<inv[i].getNomObjet()<<" ";
+        cout<<"Bonus vie: +"<<inv[i].getVieObjet()<<" ";
+        cout<<"Bonus vitesse: +"<<inv[i].getVitesseObjet()<<" ";
+        cout<<"Bonus force: +"<<inv[i].getForceObjet()<<" ";
+        cout<<"Bonus endurance: +"<<inv[i].getEnduranceObjet()<<" ";  
+    }
+
+    // AFFICHER INVENTAIRE
+    void Inventaire::afficherInventaire() const
+    {
+        for(int i = 0; i < taille_inventaire; i++)
+        {
+            if(inv[i].getNomObjet() == "emplacement vide")
             {
-                taille_inventaire = 5;
-                inv = new Objet[taille_inventaire];
-
-                /* for(int i = 0; i < taille_inventaire; i++) inv[i].Objet(); */ /** ERROR **/
+                cout<<inv[i].getNomObjet()<<endl;
             }
-
-        // CONSTRUCTEUR PAR COPIE
-            Inventaire::Inventaire(const Inventaire &in)
+            else
             {
-                taille_inventaire = in.taille_inventaire;
-                /* for(int i = 0; i < taille_inventaire; i++) inv[i].Objet(in.inv[i]); */ /** ERROR **/
+                cout<<i+1<<". ";
+                afficherObjetInventaire(i);
+                cout<<endl;
             }
+        }
+    }
 
-        // DESTRUCTEUR
-            Inventaire::~Inventaire()
+    // JETER UN OBJET
+    void Inventaire::jeterObjetInventaire(const unsigned char &i)
+    {
+       assert(i > 0);
+        assert(i < taille_inventaire);
+
+        if(i != taille_inventaire-1)
+        {
+            for(int j = i; j < taille_inventaire-1; j++)
             {
-                viderInventaire();
-                delete [] inv;
-            }
-
-    /** GESTION DE L'INVENTAIRE **/
-
-        // AFFICHER INVENTAIRE
-            void Inventaire::afficherInventaire()
-            {
-                for(int i = 0; i < taille_inventaire; i++)
-                {
-                    if(inv[i].getNomObjet() == "emplacement vide")
-                    {
-                        cout<<inv[i].getNomObjet()<<endl;
-                    }
-                    else
-                    {
-                        cout<<i<<". ";
-                        inv[i].afficherObjet();
-                    }
-                }
-            }
-
-        // VIDER INVENTAIRE
-            void Inventaire::viderInventaire()
-            {
-                for(int i = 0; i < taille_inventaire; i++) inv[i].jeterObjet();
-            }
-
-        // AJOUTER UN OBJET
-            void Inventaire::ajouterObjetInventaire(const Objet &o)
-            {
-                int i = 0;
-                char reponse;
-
-                while(inv[i].getNomObjet() != "emplacement vide" && i < 5) i++;
-
-                if(inv[i].getNomObjet() != "emplacement vide")
-                {
-                    cout<<"Inventaire plein!"<<endl;
-                    cout<<"Voulez-vous jeter un objet [o/n]? Attention l'objet jeté va disparaitre"<<endl;
-                    cin>>reponse;
-
-                        if(reponse == 'o')
-                        {
-                            afficherInventaire();
-
-                            cout<<"Quel objet voulez-vous jeter en échange de l'objet "<<inv[i].getNomObjet()<<" ?"<<endl;
-                            cin>>i;
-
-                            inv[i].prendreObjet(o);
-                        }
-                        else if (reponse == 'n')
-                        {
-                            cout<<"vous gardez l'objet "<<inv[i].getNomObjet()<<endl;
-                        }
-                }
-                else
-                {
-                    inv[i].prendreObjet(o);
-                }
-
+                inv[j].setNomObjet(inv[j+1].getNomObjet());
+                inv[j].setVieObjet(inv[j+1].getVieObjet());
+                inv[j].setVitesseObjet(inv[j+1].getVitesseObjet());
+                inv[j].setForceObjet(inv[j+1].getForceObjet());
+                inv[j].setEnduranceObjet(inv[j+1].getEnduranceObjet());
 
             }
+        }
+        
+        inv[taille_inventaire-1].setNomObjet("emplacement vide");
+        inv[taille_inventaire-1].setVieObjet(0);
+        inv[taille_inventaire-1].setVitesseObjet(0);
+        inv[taille_inventaire-1].setForceObjet(0);
+        inv[taille_inventaire-1].setEnduranceObjet(0);
+    }
 
-        // JETER UN OBJET
-            void Inventaire::jeterObjetInventaire(const unsigned int &i)
-            {
-                if((i < 0) && (i > taille_inventaire))
-                {
-                    cout<<"Impossible !"<<endl;
-                }
-                else
-                {
-                    if(i != taille_inventaire-1)
-                    {
-                        for(int j = i; j < taille_inventaire-1; j++) inv[i] = inv[i+1];
-                    }
-                     inv[taille_inventaire-1].jeterObjet();
-                }
-            }
-
-        // AFFICHER I-EME OBJET
-            void Inventaire::afficherObjetInventaire(const unsigned int &i)
-            {
-                if((i > 0) && (i < taille_inventaire))
-                {
-                    inv[i].afficherObjet();
-                }
-                else
-                {
-                    cout<<"Impossible !"<<endl;
-                }
-            }
-
-        // RECHERCHER UN OBJET
-            void Inventaire::rechercherObjetInventaire(const string &nomObjet)
-            {
-                int i = 0;
-
-                while(i < taille_inventaire && nomObjet != inv[i].getNomObjet())
-                {
-                    i++;
-                }
-
-                if(i == taille_inventaire-1 && nomObjet != inv[i].getNomObjet())
-                {
-                    cout<<"vous ne possedez pas cet objet."<<endl;
-                }
-                else
-                {
-                    cout<<"vous possedez cet objet !"<<endl;
-                }
-            }
-
-        // UTILISER UN OBJET
-            void Inventaire::utiliserObjetInventaire(const unsigned int &i)
-            {
-                assert(i > 0);
-                assert(i < taille_inventaire);
-
-                inv[i].utiliserObjet();
-            }
