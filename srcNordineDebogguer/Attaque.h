583e814e92e5b478aa1b23429bd5fce5c3b4ff66
SIDKI NOUREDDINE p2008210 <noureddine.sidki@etu.univ-lyon1.fr>
2022-03-29 19:18:46 +0000

    Upload New File

---
+++ srcNordineDebogguer/Attaque.h
@@ -0,0 +1,34 @@
+#ifndef ATTAQUE_H_INCLUDED
+#define ATTAQUE_H_INCLUDED
+#include <string>
+
+using namespace std;
+
+class Attaque
+{
+private :
+    string nomAttaque;
+    int degats;
+    string typeAttaque;
+    int nombreMaxUtilisation;
+    int TabStatsAttMagique[3];
+public :
+    Attaque();
+    Attaque(string nomAtt,int deg,int nombreAttMax,string nomTypeAtt, int TabStatsAttMag[3]);
+    ~Attaque();
+    // ACCESSEUR NOM ATTAQUE
+    string getNomAttaque();
+    // ACCESSEUR DEGATS ATTAQUE
+    int getDegats();
+    // ACCESSEUR TYPE ATTAQUE
+    string getTypeAttaque();
+    // ACCESSEUR NOMBR UTILISATION ATTAQUE
+    int getNombreMaxUtilisation();
+    void setNomAttaque(string nomAtt);
+    void setDegats(int deg);
+    void setNombreMaxUtilisation(int nombreAttMax);
+    void setTypeAttaque(string nomTypeAtt);
+    int UtiliserAttaque();
+};
+
+#endif // ATTAQUE_H_INCLUDED
