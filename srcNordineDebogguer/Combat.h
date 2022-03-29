8ccc4ab80fdf35cf713bc30508beeda3839c5c3a
SIDKI NOUREDDINE p2008210 <noureddine.sidki@etu.univ-lyon1.fr>
2022-03-29 19:19:06 +0000

    Upload New File

---
+++ srcNordineDebogguer/Combat.h
@@ -0,0 +1,26 @@
+#ifndef _COMBAT_H_
+#define _COMBAT_H_
+#include "Joueur.h"
+
+
+class Combat{
+	private:
+		Joueur *Perso;
+		Joueur *IA;
+		bool tour;
+		bool fini;
+		unsigned int tempsLimiteParTour;
+
+	public:
+		Combat();
+		Combat(Joueur *Perso_, Joueur *IA_);
+		~Combat();
+		void tourDuPerso();
+		void tourIA();
+		void combatDeroulement();
+		void TraiterActionAttaque(int degats);
+		void TraiterActionObjet(int tabStats[3]);
+
+};
+
+#endif
