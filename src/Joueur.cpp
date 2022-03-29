497ba6330ace0a47c7ebf44cb6f717649bfada4d
ITJI AMINE p2018984 <amine.itji@etu.univ-lyon1.fr>
2022-03-29 09:59:16 +0000

    Replace Joueur.cpp

--- src/Joueur.cpp
+++ src/Joueur.cpp
@@ -1,40 +1,33 @@
-#include <iostream>
 #include "Joueur.h"
+#include <iostream>
 
 using namespace std;
+
 
 // LES CONSTRUCTEURS ET DESTRUCTEUR
 
     // CONSTRUCTEUR PAR DEFAUT
-    Joueur::Joueur()
+    Joueur()
     {
-        nom = "Perso1";
-        vie = 20; // 20/20
-        vitesse = force = endurance = 10; // 10/20
+        nomJoueur = "Monkey D. Nordin";
 
+        stat = new Statistique;
         inv = new Inventaire;
         arm = new Arme;
     }
 
     // CONSTRUCTEUR PERSONNALISÉ
-    Joueur::Joueur(const string &n, const unsigned char &vi, const unsigned char &f, const unsigned char &v, const unsigned char &e, const Inventaire &in, const Arme &ar)
+    Joueur(const string &n, const unsigned int &vi, const unsigned int &f, const unsigned int &v, const unsigned int &e, const Inventaire &in, const Arme &ar)
     {
-        nom = n;
-        vie = vi;
-        vitesse = v;
-        force = f;
-        endurance = e;
-
-        // récuperer inventaire
-        // récuperer arme
+            // à finir
     }
 
     // DESTRUCTEUR
-    Joueur::~Joueur()
+    ~Joueur()
     {
-        nom = " ";
-        vie = vitesse = force = endurance = 0;
+        nomJoueur = "";
 
+        delete stat;
         delete inv;
         delete arm;
     }
@@ -42,81 +35,34 @@ using namespace std;
 // LES ACCESSEURS ET MUTATEURS 
 
     // ACCESSEUR NOM
-    const string &Joueur::getNom() const { return nom; }
-
-    // ACCESSEUR VIE
-    const unsigned char &Joueur::getVie() const { return vie; }
-
-    // ACCESSEUR FORCE
-    const unsigned char &Joueur::getForce() const { return force; }
-
-    // ACCESSEUR VITESSE
-    const unsigned char &Joueur::getVitesse() const { return vitesse; }
-
-    // ACCESSEUR ENDURANCE
-    const unsigned char &Joueur::getEndurance() const { return endurance; }
+    const string &getNom() const { return nomJoueur; }
 
     // MUTATEUR NOM
-    void Joueur::setNom(const string& n) { nom = n; }
-
-    // MUTATEUR VIE
-    void Joueur::setVie(const unsigned char &vi) { vie = vi; }
-
-    // MUTATEUR FORCE
-    void Joueur::setForce(const unsigned char &f) { force = f; }
-
-    // MUTATEUR VITESSE
-    void Joueur::setVitesse(const unsigned char &v) { vitesse = v; }
-
-    // MUTATEUR ENDURANCE
-    void Joueur::setEndurance(const unsigned char &e) { endurance = e; }
+    void setNom(const string& n) { nomJoueur = n; }
 
 // GESTION DU JOUEUR 
 
-    // REGARDER L'INVENTAIRE
-    void Joueur::regarderInventaire() const
+    // CHOIX DE L'ACTION
+    int choixAction()
     {
+        int i;
 
+        cout<<"Que voulez-vous faire ?"<<endl;
+        cout<<"1. Utiliser un Objet 2. Utiliser arme"<<endl;
+        cin>>i;
+
+        return i;
     }
 
-    // REGARDER UN OBJET ET SES STATS
-    void Joueur::regarderObjet(const unsigned char &i) const
+    // OPERATEUR << POUR AFFICHER TOUTES LES STATS
+    void operator<<()
     {
+        cout<<"******* STAT DU JOUEUR ******* "<<endl;
+        stat.afficherStats();
 
-    }
+        cout<<"******* INVENTAIRE ******* "<<endl;
+        inv.afficherInventaire();
 
-    // UTLISER UN OBJET
-    void Joueur::utiliserObjet(const unsigned char &i)
-    {
-
-    }
-
-    // REGARDER LES ATTAQUES
-    void Joueur::regarderLesAttaques() const
-    {
-
-    }
-
-    // REGARDER UNE ATTAQUE ET SES STATS
-    void Joueur::regarderUneAttaque(const unsigned char &i) const
-    {
-
-    }
-
-    // ATTAQUER
-    void Joueur::utiliserAttaque(const unsigned char &i)
-    {
-
-    }
-
-    // REGARDER SON ARME ET SES STAtS
-    void Joueur::regarderArme() const
-    {
-
-    }
-
-    // REGARDER SON ARMURE ET SES STATS
-    void Joueur::regarderArmure() const
-    {
-        
-    }
+        cout<<"******* ARME ******* "<<endl;
+        arm.afficher();
+    } 
