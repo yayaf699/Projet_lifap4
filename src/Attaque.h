0d501ddfe79907402be5d92355dc0a9e91850493
ITJI AMINE p2018984 <amine.itji@etu.univ-lyon1.fr>
2022-04-01 22:05:02 +0000

    Replace Attaque.h

--- src/Attaque.h
+++ src/Attaque.h
@@ -6,31 +6,55 @@ using namespace std;
 
 class Attaque
 {
+
+public :
+
+    Attaque();
+
+
+    Attaque(string nomAtt,int deg,int nombreAttMax,string nomTypeAtt, int TabStatsAttMag[3],string descriptionAtt);
+
+
+    //~Attaque();
+
+    // ACCESSEUR NOM ATTAQUE
+    string getNomAttaque();
+
+    // ACCESSEUR DEGATS ATTAQUE
+    int getDegats();
+
+    // ACCESSEUR TYPE ATTAQUE
+    string getTypeAttaque();
+
+    // ACCESSEUR NOMBR UTILISATION ATTAQUE
+    int getNombreMaxUtilisation();
+
+
+    string getDescAttaque();
+
+
+    void setDescAttaque(string descAttaque_);
+
+
+    void setNomAttaque(string nomAtt);
+
+
+    void setDegats(int deg);
+
+
+    void setNombreMaxUtilisation(int nombreAttMax);
+
+
+    void setTypeAttaque(string nomTypeAtt);
+
 private :
+
     string nomAttaque;
     int degats;
     string typeAttaque;
     string descAttaque;
     int nombreMaxUtilisation;
     int TabStatsAttMagique[3];
-public :
-    Attaque();
-    Attaque(string nomAtt,int deg,int nombreAttMax,string nomTypeAtt, int TabStatsAttMag[3],string descriptionAtt);
-    //~Attaque();
-    // ACCESSEUR NOM ATTAQUE
-    string getNomAttaque();
-    // ACCESSEUR DEGATS ATTAQUE
-    int getDegats();
-    // ACCESSEUR TYPE ATTAQUE
-    string getTypeAttaque();
-    // ACCESSEUR NOMBR UTILISATION ATTAQUE
-    int getNombreMaxUtilisation();
-    string getDescAttaque();
-    void setDescAttaque(string descAttaque_);
-    void setNomAttaque(string nomAtt);
-    void setDegats(int deg);
-    void setNombreMaxUtilisation(int nombreAttMax);
-    void setTypeAttaque(string nomTypeAtt);
 };
 
 #endif // ATTAQUE_H_INCLUDED
