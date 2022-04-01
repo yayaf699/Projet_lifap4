fe4370ce0eb8a480213bd897a3ee0ce5b9baef71
ITJI AMINE p2018984 <amine.itji@etu.univ-lyon1.fr>
2022-04-01 22:04:27 +0000

    Replace Combat.h

--- src/Combat.h
+++ src/Combat.h
@@ -4,23 +4,40 @@
 
 
 class Combat{
-	private:
-		Joueur Perso;
-		Joueur IA;
-		bool tour;
-		bool fini;
-		unsigned int tempsLimiteParTour;
 
-	public:
-		Combat();
-		Combat(const Joueur &Perso_, const Joueur &IA_);
-		//~Combat();
-		void tourDuPerso();
-		void tourIA();
-		void combatDeroulement();
-		void TraiterActionAttaque(int degats);
-		void DecisionIa();
+public:
 
+	//CONSTRUCTEUR
+	Combat();
+
+	//CONSTRUCTEUR PAR COPIE
+	Combat(const Joueur &Perso_, const Joueur &IA_);
+
+
+	//~Combat();
+
+	
+	void tourDuPerso();
+
+
+	void tourIA();
+
+
+	void combatDeroulement();
+
+
+	void TraiterActionAttaque(int degats);
+
+
+	void DecisionIa();
+
+private:
+
+	Joueur Perso;
+	Joueur IA;
+	bool tour;
+	bool fini;
+	unsigned int tempsLimiteParTour;
 
 };
 
