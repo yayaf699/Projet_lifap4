78c12589a19c248e302537a3aab073dc93c81676
SIDKI NOUREDDINE p2008210 <noureddine.sidki@etu.univ-lyon1.fr>
2022-04-01 20:27:56 +0000

    Replace Joueur.cpp

--- src/Joueur.cpp
+++ src/Joueur.cpp
@@ -1,142 +1,19 @@
-#include "Joueur.h"
 #include <iostream>
-#include <string.h>
-
+#include "Joueur.h"
+#include "Partie.h"
+#include "Combat.h"
+#include "Statistiques.h"
+#include "Arme.h"
+#include "Inventaire.h"
+#include "Attaque.h"
+#include "Objet.h"
+#include "Menu.h"
 
 using namespace std;
 
-
-// LES CONSTRUCTEURS ET DESTRUCTEUR
-
-
-    // CONSTRUCTEUR PAR DEFAUT
-    Joueur::Joueur()
-    {
-        nomJoueur = "Monkey D. Nordin";
-
-
-        // stat.afficherStat();
-        // cout << arm.getNomArme() << endl;
-        // arm.GetAttaques();
-
-
-    }
-
-    // CONSTRUCTEUR PERSONNALISÉ
-    Joueur::Joueur(const string &n, const unsigned int &vi, const unsigned int &f, const unsigned int &v, const unsigned int &e, Inventaire in, const Arme &ar)
-    {
-            // à finir
-    }
-
-
-// LES ACCESSEURS ET MUTATEURS
-
-    // ACCESSEUR NOM
-    const string &Joueur::getNom() const { return nomJoueur; }
-
-    // MUTATEUR NOM
-    void Joueur::setNom(const string& n) { nomJoueur = n; }
-
-    void Joueur::setStat(const string &s, int valeur)
-    {   int v;
-        if(s =="vie") v = 1;
-        if(s == "force") v = 2;
-        if(s == "vitesse") v = 3;
-
-        switch(v){
-            case 1:
-            stat.setVie(stat.getVie() + valeur);
-            break;
-
-            case 2:
-            stat.setForce(stat.getForce() + valeur);
-            break;
-
-            case 3:
-            stat.setVitesse(stat.getVitesse() + valeur);
-            break;
-
-            default:
-            break;
-
-        }
-    }
-
-// GESTION DU JOUEUR
-
-    // CHOIX DE L'ACTION
-    int Joueur::choixAction()
-    {
-        int i;
-
-        cout<<"Que voulez-vous faire ?"<<endl;
-        cout<<"1. Utiliser un Objet 2. Utiliser arme"<<endl;
-        cin>>i;
-
-        return i;
-    }
-
-    // OPERATEUR << POUR AFFICHER TOUTES LES STATS
-    void Joueur::afficherJoueur()
-    {
-         cout<<"\n ******* STAT DU JOUEUR ******* \n"<<endl;
-         stat.afficherStat();
-
-        cout<<" \n ******* INVENTAIRE ******* \n"<<endl;
-        inv.afficherInventaire();
-
-        cout<<" \n \n******* ARME ******* \n"<<endl;
-        arm.afficher();
-    }
-
-    Joueur &Joueur::operator=(const Joueur &j){
-        stat = j.stat;
-        inv = j.inv;
-        arm = j.arm;
-
-        return *this;
-    }
-
-    int  Joueur::getVie()
-    {
-        return stat.getVie();
-    }
-
-    void Joueur::SetVie(int valeur)
-    {
-        stat.setVie(valeur);
-    }
-
-    Inventaire Joueur::getInv(){
-        return inv;
-    }
-
-    Arme Joueur::getArme()
-    {
-        return arm;
-    }
-
-    Statistiques Joueur::getStats()
-    {
-        return stat;
-    }
-
-
-    int Joueur::Attaquer(int i)
-    {
-        return arm.utiliserAttaque(i);
-    }
-
-    void Joueur::afficherInventaire() const
-    {
-        inv.afficherInventaire();
-    }
-
-    // UTILISER L'OBJET
-    void Joueur::utiliserObjet(Objet o)
-    {
-        setStat("vie", o.getVieObjet());
-        setStat("force", o.getForceObjet());
-        setStat("vitesse", o.getVitesseObjet());
-        inv.retirerObjetInventaire(o);
-    }
+int main(){
+    Joueur a, b;
+    Combat c(a, b);
+    c.combatDeroulement();
+    return 0;
+}
