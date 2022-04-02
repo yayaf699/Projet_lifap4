c0080468a116f0613f5a840163bbff390d5f9f63
SIDKI NOUREDDINE p2008210 <noureddine.sidki@etu.univ-lyon1.fr>
2022-04-02 12:12:49 +0000

    Replace Inventaire.cpp

--- src/Inventaire.cpp
+++ src/Inventaire.cpp
@@ -53,13 +53,16 @@ using namespace std;
     }
 
     // RECHERCHER OBJET
-     unsigned int Inventaire::rechercherObjetInventaire(Objet o)
+    int Inventaire::rechercherObjetInventaire(string nomObjet)
     {
         for( unsigned int i = 0; i < taille_inventaire; i++){
-            if(inventaire[i].getNomObjet() == o.getNomObjet())
+            if(inventaire[i].getNomObjet() == nomObjet)
             {
                 return i;
             }
+            else {
+                    return -1;
+                    }
 
         }
     }
@@ -68,10 +71,10 @@ using namespace std;
     // JETER UN OBJET
     void Inventaire::retirerObjetInventaire(Objet &o)
     {
-        inventaire[rechercherObjetInventaire(o)].setVieObjet(0);
-        inventaire[rechercherObjetInventaire(o)].setVitesseObjet(0);
-        inventaire[rechercherObjetInventaire(o)].setForceObjet(0.0);
-        inventaire[rechercherObjetInventaire(o)].setNomObjet("emplacement vide");
+        inventaire[rechercherObjetInventaire(o.getNomObjet())].setVieObjet(0);
+        inventaire[rechercherObjetInventaire(o.getNomObjet())].setVitesseObjet(0);
+        inventaire[rechercherObjetInventaire(o.getNomObjet())].setForceObjet(0.0);
+        inventaire[rechercherObjetInventaire(o.getNomObjet())].setNomObjet("emplacement vide");
 
     }
 
