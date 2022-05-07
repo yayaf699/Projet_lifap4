2f6cfb1d6276823b8a19ca387c95f8c846573666
ITJI AMINE p2018984 <amine.itji@etu.univ-lyon1.fr>
2022-05-07 09:05:27 +0000

    Replace Arme.cpp

--- src/Arme.cpp
+++ src/Arme.cpp
@@ -9,107 +9,33 @@ using namespace std;
     // CONSTRUCTEUR
     Arme::Arme()
     {
-        nomArme = "Nordin equipement";
         nbAttaque = 4;
         tabAttaque = new Attaque[nbAttaque];
-        tabAttaque[0].setDegats(15);
-        tabAttaque[0].setNomAttaque("Pistolet");
-        tabAttaque[0].setDescAttaque("Tir de pistolet, forte chance d'infliger saignement a l'adversaire");
-        tabAttaque[0].setTypeDegats("saignement");
-        tabAttaque[1].setDegats(40);
-        tabAttaque[1].setNomAttaque("Uppercut");
-        tabAttaque[1].setNombreMaxUtilisation(3);
-        tabAttaque[1].setTypeDegats("Puissante");
-        tabAttaque[1].setDescAttaque("Coup de poings dans le menton");
-        tabAttaque[2].setDegats(0);
-        tabAttaque[2].setNomAttaque("Ice Grenade");
-        tabAttaque[2].setTypeDegats("gele");
-        tabAttaque[2].setNombreMaxUtilisation(1);
-        tabAttaque[2].setDescAttaque("Jete une grenade de glace, forte chance de geler l'adversaire");
-        tabAttaque[3].setDegats(0);
-        tabAttaque[3].setNomAttaque("Invincibilite");
-        tabAttaque[3].setTypeDegats("invincible");
-        tabAttaque[3].setNombreMaxUtilisation(1);
-        tabAttaque[3].setDescAttaque("Active son bouclier pour ne subir aucun degats pendant 2 tours");
-    }
-
-    // CONSTRUCTEUR PAR COPIE
-    Arme::Arme(const string &n, const int &nb, Attaque tab[])
-    {
-        nomArme = n;
-        nbAttaque = nb;
-
-        for(int i = 0; i < nbAttaque; i++)
-        {
-            tabAttaque[i] = tab[i];
-        }
     }
 
     // DESTRUCTEUR
     Arme::~Arme()
     {
-        nomArme = "";
         nbAttaque = 0;
-
         tabAttaque = NULL;
+
         delete [] tabAttaque;
     }
 
 // ACCESSEURS ET MUTATEURS
 
-    // ACCESSEUR NOM
-    const string &Arme::getNomArme() const
-    {
-        return nomArme;
-    }
+    // ACCESSEUR NOMBRE D'ATTAQUE
+    int Arme::getNbAttaque() { return nbAttaque; }
 
-    // MUTATEUR NOM
-    void Arme::setNomArme(const string &n)
-    {
-        nomArme = n;
-    }
+    // ACCESSEUR ATTAQUE
+    string Arme::getNomAttaque(int i) { return tabAttaque[i].getNomAttaque(); }
 
-    void Arme::affAttaques()
-    {
-        for (int i=0; i<4; i++)
-        {
-            cout<< i+1 << ": " << tabAttaque[i].getNomAttaque() << "\n";
-        }
-
-    }
-
+    // MUTATEUR ATTAQUE
+    void Arme::setNomAttaque(const unsigned int &i, const string &n) { tabAttaque[i].setNomAttaque(n); }
 
 // GESTION ARME
 
-    // AFFICHER LES ATTAQUES
-    void Arme::afficher() const
-    {
-        cout<<"NOM DE L'ARME: "<<nomArme<<"\n"<<endl;
-        for(int i = 0; i < nbAttaque; i++)
-        {
-            //  nom degats degatsSpeciaux typeDegats typeAttaque descAttaque nombreMaxUtilisation etatNombreTour
-            cout<<i+1<<". "<<endl;
-            cout<<"NOM D'ATTAQUE : "<<tabAttaque[i].getNomAttaque()<<endl;
-            cout<<"DEGATS :"<<tabAttaque[i].getDegats()<<endl;
-            cout<<"DEGATS SPECIAUX :"<<tabAttaque[i].getDegatSp()<<endl;
-            cout<<"TYPE DE DEGATS :"<<tabAttaque[i].getTypeDegats()<<endl;
-            cout<<"TYPE DE L'ATTAQUE :"<<tabAttaque[i].getTypeAttaque()<<endl;
-            cout<<"DESCRIPTION D'ATTAQUE :"<<tabAttaque[i].getDescAttaque()<<endl;
-            cout<<"NOMBRE MAX D'UTILISATION :"<<tabAttaque[i].getNombreMaxUtilisation()<<endl;
-            cout<<"ETAT NOMBRE DE TOURS :"<<tabAttaque[i].getEtatNombreTour()<<endl;
-        }
-    }
-
-    Attaque Arme::utiliserAttaque(int i){
-        tabAttaque[i].setNombreMaxUtilisation(tabAttaque[i].getNombreMaxUtilisation()-1);
-        return getAtk(i);
-    }
-
-     Attaque Arme::getAtk(int i)
-    {
-        return tabAttaque[i];
-    }
-
+    // AJOUTER ATTAQUE
     void Arme::ajouterAttaque(int indice, string nomAttaque, int degats, int degatsSpeciaux, string typeDegats, string typeAttaque, string descAttaque, int nombreMaxUtilisation, int etatNombreTour)
     {
         tabAttaque[indice].setNomAttaque(nomAttaque);
@@ -123,3 +49,32 @@ using namespace std;
     }
 
 
+    // RETOURNE I-EME ATTAQUE
+    Attaque Arme::retourneAttaqueArme(const unsigned int &i)
+    {
+        return tabAttaque[i];
+    }
+
+    // AFFICHER LES STATS DE TOUTES LES ATTAQUES
+    void Arme::afficherStat()
+    {
+        for(int i = 0; i < nbAttaque; i++)
+        {
+            cout<<i+1<<". "<<endl;
+            cout<<"NOM D'ATTAQUE : "<<tabAttaque[i].getNomAttaque()<<endl;
+            cout<<"DEGATS :"<<tabAttaque[i].getDegats()<<endl;
+            cout<<"DEGATS SPECIAUX :"<<tabAttaque[i].getDegatSp()<<endl;
+            cout<<"TYPE DE DEGATS :"<<tabAttaque[i].getTypeDegats()<<endl;
+            cout<<"TYPE DE L'ATTAQUE :"<<tabAttaque[i].getTypeAttaque()<<endl;
+            cout<<"DESCRIPTION D'ATTAQUE :"<<tabAttaque[i].getDescAttaque()<<endl;
+            cout<<"NOMBRE MAX D'UTILISATION :"<<tabAttaque[i].getNombreMaxUtilisation()<<endl;
+            cout<<"ETAT NOMBRE DE TOURS :"<<tabAttaque[i].getEtatNombreTour()<<endl;
+        }
+    }
+
+    // L'UTILISATION DE L'ATTAQUE
+    Attaque Arme::utiliserAttaque(int i)
+    {
+        tabAttaque[i].setNombreMaxUtilisation(tabAttaque[i].getNombreMaxUtilisation()-1);
+        return retourneAttaqueArme(i);
+    }
